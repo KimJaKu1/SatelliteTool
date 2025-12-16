0153d650df345794cda12186dcbe33d93eeec442
KimJaKu1 <118945992+KimJaKu1@users.noreply.github.com>
2025-12-16 17:31:19 +0900

    no message

---
+++ SatTool/src/main/java/org/example/sattool/SatToolApplication.java
@@ -0,0 +1,13 @@
+package org.example.sattool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SatToolApplication.class, args);
+    }
+
+}
---
+++ SatTool/src/test/java/org/example/sattool/SatToolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sattool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
